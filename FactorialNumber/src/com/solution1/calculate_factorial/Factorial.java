cbfdbf5c85a0249b1effb92e9240940b44656273
Varun <varunvora24@gmail.com>
2023-03-20 00:36:31 -0400

    Adding comments to make the code understandable

--- FactorialNumber/src/com/solution1/calculate_factorial/Factorial.java
+++ FactorialNumber/src/com/solution1/calculate_factorial/Factorial.java
@@ -3,26 +3,39 @@ package com.solution1.calculate_factorial;
 import java.util.Scanner;
 
 public class Factorial {
-	 public static void main(String[] args) {
-	        Scanner scanner = new Scanner(System.in);
-	        System.out.print("Enter a number ");
-	        int num = scanner.nextInt();
-	        scanner.close();
-	        
-	        if (num < 0) {
-	            System.out.println("Invalid input");
-	            return;
-	        }
-	        
-	        long result = factorial(num);
-	        System.out.println("The factorial of " + num + " is " + result);
-	    }
-	    
-	    public static long factorial(int n) {
-	        if (n == 0 || n == 1) {
-	            return 1;
-	        } else {
-	            return n * factorial(n - 1);
-	        }
-	    }
+	
+	public static void main(String[] args) {
+		// Create a scanner object to take input from the user
+		Scanner scanner = new Scanner(System.in);
+		// Prompt the user to enter a number
+		System.out.print("Enter a number ");
+		// Read the number entered by the user
+		int num = scanner.nextInt();
+		// Close the scanner
+		scanner.close();
+
+		    // Check if the entered number is negative
+		    if (num < 0) {
+		        // Print an error message and return
+		        System.out.println("Invalid input");
+		        return;
+		    }
+
+		    // Calculate the factorial of the entered number
+		    long result = factorial(num);
+		    // Print the result
+		    System.out.println("The factorial of " + num + " is " + result);
+		}
+
+		// A recursive method to calculate the factorial of the given number n
+		public static long factorial(int n) {
+		    // Check if the input is 0 or 1
+		    if (n == 0 || n == 1) {
+		        // If so, return 1
+		        return 1;
+		    } else {
+		        // Otherwise, call the factorial method recursively
+		        return n * factorial(n - 1);
+		    }
+		}
 }
--- FactorialNumber/src/com/solution1/calculate_factorial/FactorialBigInteger.java
+++ FactorialNumber/src/com/solution1/calculate_factorial/FactorialBigInteger.java
@@ -4,32 +4,45 @@ import java.math.BigInteger;
 import java.util.Scanner;
 
 public class FactorialBigInteger {
+	// A static method to calculate factorial of the given number n
 	public static BigInteger factorial(int n) {
-        if (n < 0) {
-            return null;
-        }
-        if (n == 0) {
-            return BigInteger.ONE;
-        }
-        BigInteger result = BigInteger.ONE;
-        for (int i = 1; i <= n; i++) {
-            result = result.multiply(BigInteger.valueOf(i));
-        }
-        return result;
-    }
+	    // If the input is a negative number, return null
+	    if (n < 0) {
+	        return null;
+	    }
+	    // If the input is 0, return 1
+	    if (n == 0) {
+	        return BigInteger.ONE;
+	    }
+	    // Initialize a BigInteger variable with value 1
+	    BigInteger result = BigInteger.ONE;
+	    // Loop from 1 to n and calculate factorial
+	    for (int i = 1; i <= n; i++) {
+	        result = result.multiply(BigInteger.valueOf(i));
+	    }
+	    // Return the final result
+	    return result;
+	}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        scanner.close();
+	public static void main(String[] args) {
+	    // Create a scanner object to take input from the user
+	    Scanner scanner = new Scanner(System.in);
+	    // Prompt the user to enter a number
+	    System.out.print("Enter a number: ");
+	    // Read the number entered by the user
+	    int number = scanner.nextInt();
+	    // Close the scanner
+	    scanner.close();
 
-        BigInteger factorial = factorial(number);
+	    // Calculate the factorial of the entered number
+	    BigInteger factorial = factorial(number);
 
-        if (factorial == null) {
-            System.out.println("Factorial is not defined for negative numbers.");
-        } else {
-            System.out.println("Factorial of " + number + " is: " + factorial);
-        }
-    }
+	    // If the factorial is null, print an error message
+	    if (factorial == null) {
+	        System.out.println("Factorial is not defined for negative numbers.");
+	    } else {
+	        // Otherwise, print the factorial
+	        System.out.println("Factorial of " + number + " is: " + factorial);
+	    }
+	}
 }
